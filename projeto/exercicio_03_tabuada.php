a02654cff98873174fc8dc32898c9459958488c6
𝐕𝐈𝐍𝐘𝐂𝐈𝐔𝐒 <vinycius.silva@edu.senai.br>
2026-08-18 13:58:57 -0300

    Delete projeto directory

--- projeto/exercicio_03_tabuada.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Número específico para a tabuada
-$numero = 7;
-
-// Laço de repetição para calcular a tabuada de 1 a 10
-for ($i = 1; $i <= 10; $i++) {
-    echo "$numero x $i = " . ($numero * $i) . "<br>";
-}
-?>
